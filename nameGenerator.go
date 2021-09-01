5e1377dfaf925952f3fb9fcafa8534a24e687f84
Grace Thompson <grace.thompson97@gmail.com>
2021-08-31 19:18:21 -0700

    get rid of comments

--- nameGenerator.go
+++ nameGenerator.go
@@ -11,46 +11,11 @@ import (
 )
 
 func main() {
-	naming(1)
 	naming(2)
-	naming(3)
-	naming(4)
+    naming(2)
+	naming(2)
 }
 func naming(syllables int) {
-	//rVIndex := r.Intn(6)
-    //vTracker := 0
-	//rCIndex := r.Intn(20)
-    //cTracker := 0
-    /*
-	var v [6]string
-	v[0] = "a"
-	v[1] = "e"
-	v[2] = "i"
-	v[3] = "o"
-	v[4] = "u"
-	v[5] = "y"
-	var con [20]string
-	con[0] = "b"
-	con[1] = "c"
-	con[2] = "d"
-	con[3] = "f"
-	con[4] = "g"
-	con[5] = "h"
-	con[6] = "j"
-	con[7] = "k"
-	con[8] = "l"
-	con[9] = "m"
-	con[10] = "n"
-	con[11] = "p"
-	con[12] = "q"
-	con[13] = "r"
-	con[14] = "s"
-	con[15] = "t"
-	con[16] = "v"
-	con[17] = "w"
-	con[18] = "x"
-	con[19] = "z"
-    */
 	name := ""
 	for i := 1; i <= syllables; i++ {
 		name = name + genSyl("")
@@ -67,7 +32,7 @@ func genSyl(nameChunk string) string {
 	defer n.Close()
 	scanner := bufio.NewScanner(n)
 	j := 0
-	var names [150]string
+	var names [1150]string
 	for scanner.Scan() {
 		names[j] = scanner.Text()
 		j++
