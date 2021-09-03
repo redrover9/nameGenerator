8c037b067d6a6324d5abc4bdc3dda9b62b8d66af
Grace Thompson <grace.thompson97@gmail.com>
2021-09-02 17:57:36 -0700

    gonna try in an alternate env

---
+++ ml2.py
@@ -0,0 +1,108 @@
+import tensorflow as tf
+from tensorflow.keras.layers.experimental import preprocessing
+
+import numpy as np
+import os
+import time
+path_to_file = tf.keras.utils.get_file('namefile.txt', 'https://raw.githubusercontent.com/redrover9/nameGenerator/master/fakeNames.txt')
+# Read, then decode for py2 compat.
+text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+# length of text is the number of characters in it
+print(f'Length of text: {len(text)} characters')
+# Take a look at the first 250 characters in text
+print(text[:250])
+# The unique characters in the file
+vocab = sorted(set(text))
+print(f'{len(vocab)} unique characters')
+example_texts = ['abcdefg', 'xyz']
+
+chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
+chars
+ids_from_chars = preprocessing.StringLookup(
+    vocabulary=list(vocab), mask_token=None)
+ids = ids_from_chars(chars)
+ids
+chars_from_ids = tf.keras.layers.experimental.preprocessing.StringLookup(
+    vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
+chars = chars_from_ids(ids)
+chars
+tf.strings.reduce_join(chars, axis=-1).numpy()
+def text_from_ids(ids):
+  return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
+all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
+all_ids
+ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
+for ids in ids_dataset.take(10):
+    print(chars_from_ids(ids).numpy().decode('utf-8'))
+seq_length = 100
+examples_per_epoch = len(text)//(seq_length+1)
+sequences = ids_dataset.batch(seq_length+1, drop_remainder=True)
+
+for seq in sequences.take(1):
+    print(chars_from_ids(seq))
+for seq in sequences.take(5):
+    print(text_from_ids(seq).numpy())
+def split_input_target(sequence):
+    input_text = sequence[:-1]
+    target_text = sequence[1:]
+    return input_text, target_text
+split_input_target(list("Tensorflow"))
+dataset = sequences.map(split_input_target)
+for input_example, target_example in dataset.take(1):
+    print("Input :", text_from_ids(input_example).numpy())
+    print("Target:", text_from_ids(target_example).numpy())
+# Batch size
+BATCH_SIZE = 64
+
+# Buffer size to shuffle the dataset
+# (TF data is designed to work with possibly infinite sequences,
+# so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+# it maintains a buffer in which it shuffles elements).
+BUFFER_SIZE = 10000
+
+dataset = (
+    dataset
+    .shuffle(BUFFER_SIZE)
+    .batch(BATCH_SIZE, drop_remainder=True)
+    .prefetch(tf.data.experimental.AUTOTUNE))
+
+dataset
+# Length of the vocabulary in chars
+vocab_size = len(vocab)
+
+# The embedding dimension
+embedding_dim = 256
+
+# Number of RNN units
+rnn_units = 1024
+class MyModel(tf.keras.Model):
+  def __init__(self, vocab_size, embedding_dim, rnn_units):
+    super().__init__(self)
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(rnn_units,
+                                   return_sequences=True,
+                                   return_state=True)
+    self.dense = tf.keras.layers.Dense(vocab_size)
+
+  def call(self, inputs, states=None, return_state=False, training=False):
+    x = inputs
+    x = self.embedding(x, training=training)
+    if states is None:
+      states = self.gru.get_initial_state(x)
+    x, states = self.gru(x, initial_state=states, training=training)
+    x = self.dense(x, training=training)
+
+    if return_state:
+      return x, states
+    else:
+      return x
+model = MyModel(
+    # Be sure the vocabulary size matches the `StringLookup` layers.
+    vocab_size=len(ids_from_chars.get_vocabulary()),
+    embedding_dim=embedding_dim,
+    rnn_units=rnn_units)
+for input_example_batch, target_example_batch in dataset.take(1):
+    example_batch_predictions = model(input_example_batch)
+    print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+model.summary()
+
