1276bf3318807906f5c82362e35a6ba3437e0b45
Grace <grace.thompson97@gmail.com>
2021-09-03 02:23:30 +0000

    did I just fix this?

--- ml2.py
+++ ml2.py
@@ -104,5 +104,153 @@ model = MyModel(
 for input_example_batch, target_example_batch in dataset.take(1):
     example_batch_predictions = model(input_example_batch)
     print(example_batch_predictions.shape, "# (batch_size, sequence_length, vocab_size)")
+model.build(input_shape=(101, 256))
 model.summary()
+sampled_indices = tf.random.categorical(example_batch_predictions[0], num_samples=1)
+sampled_indices = tf.squeeze(sampled_indices, axis=-1).numpy()
+sampled_indices
+print("Input:\n", text_from_ids(input_example_batch[0]).numpy())
+print()
+print("Next Char Predictions:\n", text_from_ids(sampled_indices).numpy())
+loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+example_batch_loss = loss(target_example_batch, example_batch_predictions)
+mean_loss = example_batch_loss.numpy().mean()
+print("Prediction shape: ", example_batch_predictions.shape, " # (batch_size, sequence_length, vocab_size)")
+print("Mean loss:        ", mean_loss)
+tf.exp(mean_loss).numpy()
+model.compile(optimizer='adam', loss=loss)
+# Directory where the checkpoints will be saved
+checkpoint_dir = './training_checkpoints'
+# Name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
 
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+EPOCHS = 20
+history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+class OneStep(tf.keras.Model):
+  def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
+    super().__init__()
+    self.temperature = temperature
+    self.model = model
+    self.chars_from_ids = chars_from_ids
+    self.ids_from_chars = ids_from_chars
+
+    # Create a mask to prevent "[UNK]" from being generated.
+    skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
+    sparse_mask = tf.SparseTensor(
+        # Put a -inf at each bad index.
+        values=[-float('inf')]*len(skip_ids),
+        indices=skip_ids,
+        # Match the shape to the vocabulary
+        dense_shape=[len(ids_from_chars.get_vocabulary())])
+    self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+
+  @tf.function
+  def generate_one_step(self, inputs, states=None):
+    # Convert strings to token IDs.
+    input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
+    input_ids = self.ids_from_chars(input_chars).to_tensor()
+
+    # Run the model.
+    # predicted_logits.shape is [batch, char, next_char_logits]
+    predicted_logits, states = self.model(inputs=input_ids, states=states,
+                                          return_state=True)
+    # Only use the last prediction.
+    predicted_logits = predicted_logits[:, -1, :]
+    predicted_logits = predicted_logits/self.temperature
+    # Apply the prediction mask: prevent "[UNK]" from being generated.
+    predicted_logits = predicted_logits + self.prediction_mask
+
+    # Sample the output logits to generate token IDs.
+    predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+    predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+
+    # Convert from token ids to characters
+    predicted_chars = self.chars_from_ids(predicted_ids)
+
+    # Return the characters and model state.
+    return predicted_chars, states
+one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
+start = time.time()
+states = None
+next_char = tf.constant(['ROMEO:'])
+result = [next_char]
+
+for n in range(1000):
+  next_char, states = one_step_model.generate_one_step(next_char, states=states)
+  result.append(next_char)
+
+result = tf.strings.join(result)
+end = time.time()
+print(result[0].numpy().decode('utf-8'), '\n\n' + '_'*80)
+print('\nRun time:', end - start)
+start = time.time()
+states = None
+next_char = tf.constant(['ROMEO:', 'ROMEO:', 'ROMEO:', 'ROMEO:', 'ROMEO:'])
+result = [next_char]
+
+for n in range(1000):
+  next_char, states = one_step_model.generate_one_step(next_char, states=states)
+  result.append(next_char)
+
+result = tf.strings.join(result)
+end = time.time()
+print(result, '\n\n' + '_'*80)
+print('\nRun time:', end - start)
+tf.saved_model.save(one_step_model, 'one_step')
+one_step_reloaded = tf.saved_model.load('one_step')
+states = None
+next_char = tf.constant(['ROMEO:'])
+result = [next_char]
+
+for n in range(100):
+  next_char, states = one_step_reloaded.generate_one_step(next_char, states=states)
+  result.append(next_char)
+
+print(tf.strings.join(result)[0].numpy().decode("utf-8"))
+class CustomTraining(MyModel):
+  @tf.function
+  def train_step(self, inputs):
+      inputs, labels = inputs
+      with tf.GradientTape() as tape:
+          predictions = self(inputs, training=True)
+          loss = self.loss(labels, predictions)
+      grads = tape.gradient(loss, model.trainable_variables)
+      self.optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+      return {'loss': loss}
+model = CustomTraining(
+    vocab_size=len(ids_from_chars.get_vocabulary()),
+    embedding_dim=embedding_dim,
+    rnn_units=rnn_units)
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
+model.fit(dataset, epochs=1)
+EPOCHS = 10
+
+mean = tf.metrics.Mean()
+
+for epoch in range(EPOCHS):
+    start = time.time()
+
+    mean.reset_states()
+    for (batch_n, (inp, target)) in enumerate(dataset):
+        logs = model.train_step([inp, target])
+        mean.update_state(logs['loss'])
+
+        if batch_n % 50 == 0:
+            template = f"Epoch {epoch+1} Batch {batch_n} Loss {logs['loss']:.4f}"
+            print(template)
+
+    # saving (checkpoint) the model every 5 epochs
+    if (epoch + 1) % 5 == 0:
+        model.save_weights(checkpoint_prefix.format(epoch=epoch))
+
+    print()
+    print(f'Epoch {epoch+1} Loss: {mean.result().numpy():.4f}')
+    print(f'Time taken for 1 epoch {time.time() - start:.2f} sec')
+    print("_"*80)
+
+model.save_weights(checkpoint_prefix.format(epoch=epoch))
